9700ae739d5830cedddf9daac2ca1fc42494a7ea
Siyun Feng <siyunf21@gmail.com>
2024-02-15 16:54:28 -0500

    created Movies service to fetch data from API for returning to HomeComponent and displaying the data in Home page

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3/movie/upcoming?api_key=';
+  apiKey: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=29a81657e07cee53699e4fd1e78a310d');
+  }
 }
