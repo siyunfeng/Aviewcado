a5b0f1e55064e8543443cb65b51ab8387ea3aeaf
Siyun Feng <siyunf21@gmail.com>
2024-02-16 18:49:50 -0500

    removed printing data to the frontend

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,17 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss',
-  animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
+  styleUrl: './slider.component.scss'
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
-
-  curSlideIndex: number = 0;
 
   constructor() {}
 
