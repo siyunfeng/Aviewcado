007b02d5bef662e2d3712322f66c4a3ef8b55fc8
Siyun Feng <siyunf21@gmail.com>
2024-02-17 01:07:42 -0500

    set up basic content and layout for MovieItemComponent and ItemsBannerComponent

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-movie-item',
+  selector: 'movie-item',
   templateUrl: './movie-item.component.html',
   styleUrl: './movie-item.component.scss'
 })
-export class MovieItemComponent {
-
-}
+export class MovieItemComponent {}
