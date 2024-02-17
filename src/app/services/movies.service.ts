b04fd3b3cdd4f4f5142948f92bed05b42a4bdd47
Siyun Feng <siyunf21@gmail.com>
2024-02-17 01:53:11 -0500

    updated movies service HttpClient request

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -5,12 +5,12 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MoviesService {
-  baseUrl: string = 'https://api.themoviedb.org/3/movie/upcoming?api_key=';
-  apiKey: string = '';
+  baseUrl: string = 'https://api.themoviedb.org/3/';
+  apiKey: string = '29a81657e07cee53699e4fd1e78a310d';
 
   constructor(private http: HttpClient) {}
 
-  getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=29a81657e07cee53699e4fd1e78a310d');
+  getMovies(type: string = 'upcoming') {
+    return this.http.get(`${this.baseUrl}movie/${type}?api_key=${this.apiKey}`);
   }
 }
