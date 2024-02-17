1ea422cd6a417d51075b864475d8b9deec30d845
Siyun Feng <siyunf21@gmail.com>
2024-02-17 00:08:59 -0500

    added sliding effect to slide banner by using Rxjs

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
+  animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
 
-  constructor() {}
+  curSlideIndex: number = 0;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const source = interval(5000);
+    const numSlides = source.pipe(take(this.items.length - 1));
+
+    numSlides.subscribe(() => (this.curSlideIndex = ++this.curSlideIndex % this.items.length));
+  }
 }
