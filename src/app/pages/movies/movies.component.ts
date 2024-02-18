8a1892350166b69ac298b1ddc65ee1e5222e82a0
Siyun Feng <siyunf21@gmail.com>
2024-02-18 17:58:11 -0500

    added Movies page basic content and layout by reusing the template from ItemBannerComponent

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+  movies: Movie[] = [];
 
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.moviesService.getMovies('popular', 5).subscribe((movies) => (this.movies = movies));
+  }
 }
