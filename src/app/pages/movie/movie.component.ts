405d6ac510765dc7018d08b9bd5af6f193fb035d
Siyun Feng <siyunf21@gmail.com>
2024-02-20 19:46:41 -0500

    added ngOnDestory to unsubscribe by using first from rxjs

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { ActivatedRoute } from '@angular/router';
 import { Movie, Genre, MovieVideo, MovieImages, MovieCredits } from '../../models/movie';
 import { IMAGE_SIZE } from '../../constants/image-size';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.scss'
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
   movie: Movie | null = null;
   movieVideos: MovieVideo[] = [];
   movieImages: MovieImages | null = null;
@@ -19,13 +20,15 @@ export class MovieComponent implements OnInit {
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({ id }) => {
+    this.route.params.pipe(first()).subscribe(({ id }) => {
       this.getMovie(id);
       this.getMovieVideos(id);
       this.getMovieImages(id);
       this.getMovieCredits(id);
     });
   }
+
+  ngOnDestroy(): void {}
 
   getMovie(id: string) {
     this.moviesService.getMovieById(id).subscribe((data) => (this.movie = data));
