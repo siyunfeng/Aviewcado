3c2188b31d3cb1dccac50c88d5cc11a1b0f8ceee
Siyun Feng <siyunf21@gmail.com>
2024-02-23 01:45:37 -0500

    updated TvShowComponent and TvShowsComponent by reusing fixed MovieComponent and MoviesComponent

--- src/app/pages/tv-show/tv-show.component.ts
+++ src/app/pages/tv-show/tv-show.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TvshowsService } from '../../services/tv-shows.service';
+import { ActivatedRoute } from '@angular/router';
+import { TvShow, TvShowVideo, TvShowImages, TvShowCredits, convertTvShowToItem } from '../../models/tvshow';
+import { Item } from '../../models/item';
+import { IMAGE_SIZE } from '../../constants/image-size';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'tv-show',
   templateUrl: './tv-show.component.html',
   styleUrl: './tv-show.component.scss'
 })
-export class TvShowComponent {}
+export class TvShowComponent implements OnInit, OnDestroy {
+  tvShow: TvShow | null = null;
+  tvShowBanner: Item | null = null;
+  tvShowVideos: TvShowVideo[] = [];
+  tvShowImages: TvShowImages | null = null;
+  tvShowCredits: TvShowCredits | null = null;
+  similarTvShows: TvShow[] = [];
+
+  imageSize = IMAGE_SIZE;
+
+  constructor(private route: ActivatedRoute, private tvShowServices: TvshowsService) {}
+
+  ngOnInit(): void {
+    this.route.params.pipe(first()).subscribe(({ id }) => {
+      this.getTvShow(id);
+      this.getTvShowVideos(id);
+      this.getTvShowImages(id);
+      this.getTvShowCredits(id);
+      this.getSimilarTvShows(id);
+    });
+  }
+
+  ngOnDestroy(): void {}
+
+  getTvShow(id: string) {
+    this.tvShowServices.getTvShowById(id).subscribe((data) => {
+      if (data) this.tvShowBanner = convertTvShowToItem(data);
+      this.tvShow = data;
+    });
+  }
+
+  getTvShowVideos(id: string) {
+    this.tvShowServices.getTvShowVideos(id).subscribe((data) => (this.tvShowVideos = data));
+  }
+
+  getTvShowImages(id: string) {
+    this.tvShowServices.getTvShowImages(id).subscribe((data) => (this.tvShowImages = data));
+  }
+
+  getTvShowCredits(id: string) {
+    this.tvShowServices.getTvShowCredits(id).subscribe((data) => (this.tvShowCredits = data));
+  }
+
+  getSimilarTvShows(id: string) {
+    this.tvShowServices.getSimilarTvShows(id).subscribe((data) => (this.similarTvShows = data));
+  }
+}
--- src/app/pages/tv-shows/tv-shows.component.ts
+++ src/app/pages/tv-shows/tv-shows.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { TvshowsService } from '../../services/tv-shows.service';
+import { convertTvShowToItem } from '../../models/tvshow';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'tv-shows',
   templateUrl: './tv-shows.component.html',
   styleUrl: './tv-shows.component.scss'
 })
-export class TvShowsComponent {}
+export class TvShowsComponent implements OnInit {
+  tvShows: Item[] = [];
+  genreId: string | null = null;
+  searchValue: string | null = null;
+
+  constructor(private tvShowsService: TvshowsService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.pipe(take(1)).subscribe(({ genreId }) => {
+      if (genreId) {
+        this.genreId = genreId;
+        this.getTvShowsByGenre(genreId, 1);
+      } else {
+        this.getTvShowsByPage(1);
+      }
+    });
+  }
+
+  getTvShowsByPage(page: number, searchInput?: string) {
+    this.tvShowsService.searchTvShows(page, searchInput).subscribe((tvShows) => {
+      this.tvShows = tvShows.map((tvShow) => convertTvShowToItem(tvShow));
+    });
+  }
+
+  getTvShowsByGenre(genreId: string, page: number) {
+    this.tvShowsService.getTvShowsByGenre(genreId, page).subscribe((tvShows) => {
+      this.tvShows = tvShows.map((tvShow) => convertTvShowToItem(tvShow));
+    });
+  }
+
+  paginate(event: any) {
+    const pageNum = event.page + 1;
+
+    if (this.genreId) {
+      this.getTvShowsByGenre(this.genreId, pageNum);
+    } else {
+      if (this.searchValue) {
+        this.getTvShowsByPage(pageNum, this.searchValue);
+      } else {
+        this.getTvShowsByPage(pageNum);
+      }
+    }
+  }
+
+  searchInputChange() {
+    if (this.searchValue) this.getTvShowsByPage(1, this.searchValue);
+  }
+}
